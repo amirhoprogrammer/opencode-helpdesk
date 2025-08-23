75bdba52d2005cf33b3e797588bf247d0f4eb0ed
amihoprogrammer <amirhanegareshi@gmail.com>
2025-08-23 11:40:19 +0330

    add logo to project

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../public/logo-edited1.png";
 function Navbar() {
   return (
     <nav className="pb-4 border-b-2 border-gray-200 flex items-center gap-5 my-10 mx-auto max-w-5xl">
+      <Image src={Logo} alt="opencode helpdesk" className="w-34 rounded-2xl" quality={100} />
       <h2 className="font-bold text-lg" style={{ color: "#7856ff" }}>
         Opencode HelpDesk
       </h2>
