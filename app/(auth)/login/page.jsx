d6df34d2a532d12682de42e04b416fbbcb5b381c
Amirhossein Aminnegareshi <108186011+amirhoprogrammer@users.noreply.github.com>
2025-11-10 17:09:44 +0330

    add form in login & signup

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,18 @@
+"use client";
+
+import AuthForm from "../AuthForm";
+
 function Login() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log("User Login", email, password);
+  };
+
   return (
     <main>
       <h2 className="text-center">Log In</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,17 @@
+"use client";
+
+import AuthForm from "../AuthForm";
+
 function SignUp() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log("User SignUp", email, password);
+  };
   return (
     <main>
       <h2 className="text-center">Sign Up</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
