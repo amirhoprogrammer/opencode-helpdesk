69f2a6b610b6cebbff0deb5496919979b1aa2867
Amirhossein Aminnegareshi <108186011+amirhoprogrammer@users.noreply.github.com>
2025-11-12 08:59:59 +0330

    add supabase in project

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,17 +1,41 @@
 "use client";
-
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import AuthForm from "../AuthForm";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function SignUp() {
+  const [formError, setFormError] = useState("");
+  const router = useRouter("");
+
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
-    console.log("User SignUp", email, password);
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`,
+      },
+    });
+
+    //console.log("User SignUp", email, password);
+
+    if (error) {
+      setFormError(error.message);
+    }
+    if (!error) {
+      router.push("/verify");
+    }
   };
+
   return (
     <main>
       <h2 className="text-center">Sign Up</h2>
 
       <AuthForm handleSubmit={handleSubmit} />
+
+      {formError && <div className="error">{formError}</div>}
     </main>
   );
 }
---
+++ app/(auth)/verify/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Verify() {
+  return (
+    <main className="text-center">
+      <h2>Thanks for Registering!</h2>
+      <p>Before Loggin, You Need to Verify Your Email Address.</p>
+    </main>
+  );
+}
+export default Verify;
